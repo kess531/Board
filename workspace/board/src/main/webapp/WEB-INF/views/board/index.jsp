ac163d2d4dcb91bbb9ce00883526d27982cfeeae
kess531 <67779553+kess531@users.noreply.github.com>
2021-03-23 20:19:26 +0900

    Index : json Test2

--- workspace/board/src/main/webapp/WEB-INF/views/board/index.jsp
+++ workspace/board/src/main/webapp/WEB-INF/views/board/index.jsp
@@ -16,66 +16,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- fontawesome-->
     <script src="https://kit.fontawesome.com/26b2ef94cb.js" crossorigin="anonymous"></script>
-   <script defer type="text/javascript" >
-    
-    const container__board = document.querySelector('.container__board');
-    $(document).ready(function(){
-    	getBoardList();
-	});
-    
-        function getBoardList(){
-            
-            $.ajax({            
-                type:'GET',
-                url:'${pageContext.request.contextPath}/bulletin/listView',
-                dataType:'JSON',
-                success : function(obj) {
-                	console.log('성공');
-                    getBoardListCallback(obj);                
-                },           
-                error : function(xhr, status, error) {}
-             });
-        }
-        
-        function getBoardListCallback(obj){
-            
-            var list = obj;
-            var listLen = list.list.length
-            console.log(list);
-            console.log(list.list.length);
-		   if(listLen >  0){
-                
-                for(let i=0; i<listLen; i++){
-                    
-                    var bltTitle        = list.list[i].bltTitle; 
-                    var bltContent         = list.list[i].bltContent; 
-                    var bltType         = list.list[i].bltType; 
-                    var bltDate         = list.list[i].bltDate; 
-                    var bltLike     = list.list[i].bltLike; 
-                    var bltCnt     = list.list[i].bltCnt; 
-                    var memberName     = list.list[i].memberName; 
-                    console.log(bltTitle);
-                    const itemRow =document.createElement('div');
-                    itemRow.setAttribute('class','container__board__list');
-                    itemRow.innerHTML=`
-                    <span class="container__board__title">
-                    <span class="container__board__type">${bltType}</span>
-                    <a href="contentview">${bltTitle}</a> </span>
-                <span class="container__board__writer">${memberName}</span>
-                <span class="container__board__date">${bltDate}</span>
-                <span class="container__board__cnt"><i class="fas fa-eye"></i>${bltCnt}</span>
-                <span class="container__board__like"><i class="fas fa-grin-hearts"></i>${bltLike}</span>`; 
-                } 
-                
-            } else {
-                
-                console.log('게시글이없다');
-            }
-            
-            container__board.appendChild(itemRow);
-        }
-        
-    </script>
+   <script src="/resources/js/main.js" defer  ></script>
     </head>
 <body>
 
--- workspace/board/src/main/webapp/resources/js/main.js
+++ workspace/board/src/main/webapp/resources/js/main.js
@@ -1,21 +1,64 @@
 'user script';
 const arrowup__btn = document.querySelector('.arrowup__btn');
 const header__logo = document.querySelector('.header__logo');
-// 답글
-const container__contentview__comment__content__replyBtn
-=document.querySelector('.container__contentview__comment__content__replyBtn');
-const container__contentview__comment__reply
-=document.querySelector('.container__contentview__comment__reply');
-
-
-container__contentview__comment__content__replyBtn.addEventListener('click', ()=>{
-    if( container__contentview__comment__reply.style.display==='block')
-    container__contentview__comment__reply.style.display='none';
-    else{
-        container__contentview__comment__reply.style.display='block';
-    }
-});
 
 arrowup__btn.addEventListener('click',()=>{
     header__logo.scrollIntoView({behavior:"smooth"});
 })
+
+   
+const container__board = document.querySelector('.container__board');
+$(document).ready(function(){
+    getBoardList();
+});
+
+    function getBoardList(){
+        
+        $.ajax({            
+            type:'GET',
+            url:'${pageContext.request.contextPath}/bulletin/listView',
+            dataType:'JSON',
+            success : function(obj) {
+                console.log('성공');
+                getBoardListCallback(obj);                
+            },           
+            error : function(xhr, status, error) {}
+         });
+    }
+    
+    function getBoardListCallback(obj){
+        
+        var list = obj;
+        var listLen = list.list.length
+        console.log(list);
+        console.log(list.list.length);
+       if(listLen >  0){
+            
+            for(let i=0; i<listLen; i++){
+                
+                var bltTitle        = list.list[i].bltTitle; 
+                var bltContent         = list.list[i].bltContent; 
+                var bltType         = list.list[i].bltType; 
+                var bltDate         = list.list[i].bltDate; 
+                var bltLike     = list.list[i].bltLike; 
+                var bltCnt     = list.list[i].bltCnt; 
+                var memberName     = list.list[i].memberName; 
+                const itemRow =document.createElement('div');
+                itemRow.setAttribute('class','container__board__list');
+                itemRow.innerHTML=`
+                <span class="container__board__title">
+                <span class="container__board__type">${bltType}</span>
+                <a href="contentview">${bltTitle}</a> </span>
+            <span class="container__board__writer">${memberName}</span>
+            <span class="container__board__date">${bltDate}</span>
+            <span class="container__board__cnt"><i class="fas fa-eye"></i>${bltCnt}</span>
+            <span class="container__board__like"><i class="fas fa-grin-hearts"></i>${bltLike}</span>`; 
+            } 
+            
+        } else {
+            
+            console.log('게시글이없다');
+        }
+        
+        container__board.appendChild(itemRow);
+    }
